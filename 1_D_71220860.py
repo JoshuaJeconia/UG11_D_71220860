e5ea08c2b12b70b255938e93ab56cc729a7a1eb2
JoshuaJeconia <117901765+JoshuaJeconia@users.noreply.github.com>
2022-12-01 16:22:18 +0700

    Add files via upload

---
+++ 1_D_71220860.py
@@ -0,0 +1,35 @@
+print("selamat datang di bioskop !")
+print("Dimanakah anda ingin menonton ?")
+print ("[1] XXI empire")
+print ("[2] XXI Amplaz")
+print ("[3] XXI JCM")
+operasi = input('Masukan pilihanmu (1/2/3): ')
+if operasi == '1':
+  print('XXI empire')
+else operasi == '2':
+  print('XXI Amplaz')
+else operasi == '3':
+  print('XXI JCM')
+print("Mau nonton film apa nih? Ada film:")
+print ("[1] Frozen")
+print ("[2] Keramat")
+print ("[3] KKN di desa penari")
+operasi = input('Pilih nomor film: (1/2/3): ')
+if operasi == '1':
+  print('Frozen')
+else operasi == '2':
+  print('Keramat')
+else operasi == '3':
+  print('KKN di desa penari')
+print("Pilih Nomor tipe layar:")
+print ("[1] Reguler")
+print ("[2] Dolby almos")
+print ("[3] Premiere")
+operasi = input('Pilih nomor film: (1/2/3): ')
+if operasi == '1':
+  print('Reguler')
+else operasi == '2':
+  print('Dolby almos')
+else operasi == '3':
+  print('Premiere')
+print("Berapa banyak tiket?")
